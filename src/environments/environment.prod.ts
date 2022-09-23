88eace278a15cc70e374e59d67cc7f72c9d2b645
kavita-ahirwar <kavita@thoughtwin>
2022-09-23 20:02:35 +0530

    update file added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,31 @@
 export const environment = {
   production: true,
   firebaseConfig :{
-    apiKey: "AIzaSyCIrOlc5vHqULKtskrqki6OAuoqypaI_TA",
-    authDomain: "angularfirebase-authenti-a2fb1.firebaseapp.com",
-    projectId: "angularfirebase-authenti-a2fb1",
-    storageBucket: "angularfirebase-authenti-a2fb1.appspot.com",
-    messagingSenderId: "495856810840",
-    appId: "1:495856810840:web:ef9890343d52e082b0c32d",
-    measurementId: "G-1Q5Z7JGHN3"
+    // apiKey: "AIzaSyCIrOlc5vHqULKtskrqki6OAuoqypaI_TA",
+    // authDomain: "angularfirebase-authenti-a2fb1.firebaseapp.com",
+    // projectId: "angularfirebase-authenti-a2fb1",
+    // storageBucket: "angularfirebase-authenti-a2fb1.appspot.com",
+    // messagingSenderId: "495856810840",
+    // appId: "1:495856810840:web:ef9890343d52e082b0c32d",
+    // measurementId: "G-1Q5Z7JGHN3"
+
+  //   apiKey: "AIzaSyCIrOlc5vHqULKtskrqki6OAuoqypaI_TA",
+  // authDomain: "angularfirebase-authenti-a2fb1.firebaseapp.com",
+  // databaseURL: "https://angularfirebase-authenti-a2fb1-default-rtdb.firebaseio.com",
+  // projectId: "angularfirebase-authenti-a2fb1",
+  // storageBucket: "angularfirebase-authenti-a2fb1.appspot.com",
+  // messagingSenderId: "495856810840",
+  // appId: "1:495856810840:web:ef9890343d52e082b0c32d",
+  // measurementId: "G-1Q5Z7JGHN3"
+
+
+  apiKey: "AIzaSyBUgNFS13c48PtIJMzCMPT8qcbDWj12VWU",
+  authDomain: "primeng-authantication.firebaseapp.com",
+  databaseURL: "https://primeng-authantication-default-rtdb.firebaseio.com",
+  projectId: "primeng-authantication",
+  storageBucket: "primeng-authantication.appspot.com",
+  messagingSenderId: "346274216699",
+  appId: "1:346274216699:web:a3904bd29679b7a7b64e5e",
+  measurementId: "G-44F25D0S16"
   }
 };
